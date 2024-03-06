0748376019adb01b7fb1b446dcea8d68643f5a60
rahman-cs-it <siddiquewasifur786123@gmail.com>
2024-03-06 17:32:33 +0530

    Use state

---
+++ l5_react/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+let [counter,setCounter] = useState(15);
+
+
+  // let counter = 15;
+
+  let addValue = () =>{
+    console.log(counter)
+    setCounter(++counter)
+    console.log(counter)
+    if(counter>20){
+      setCounter(0);
+    }
+   
+  }
+
+  let removeValue = () =>{
+    setCounter(--counter)
+    if (counter <0){
+      setCounter(0);
+    }
+
+  }
+  return (
+    <>
+    <h1>Lets learn to code {counter} </h1>
+    <h2>counter value {counter} </h2>
+    <button onClick={addValue} >add value {counter} </button>
+    <br />
+    <button onClick={removeValue} >remove value {counter} </button>
+    </>
+  )
+}
+
+export default App
